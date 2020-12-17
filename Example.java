3eb231a9ad5e7a6d178841daa6898d3dd959b6b5
Moniga-crypto <75611898+Moniga-crypto@users.noreply.github.com>
2020-12-17 14:43:36 +0530

    Type Casting

---
+++ Example.java
@@ -0,0 +1,21 @@
+package com.hello;
+
+public class Example {
+	public static void main(String[] args) {
+//	float value =20.5f;
+	//int f= (int)value;
+		//byte a=20;
+		//byte b=10;
+		//byte c=(byte)(a+b);
+		byte b;
+		byte s;
+		int i=257;
+		double d=257.0;
+		b=(byte)i;
+		s=(byte)d;
+	System.out.println(b);
+	System.out.println(s);
+System.out.println(i);
+System.out.println(d);
+}	
+}
