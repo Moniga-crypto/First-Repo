efdff26ac10e89f6e80f1bb406245230ab556d5e
Moniga-crypto <75611898+Moniga-crypto@users.noreply.github.com>
2020-12-19 13:10:21 +0530

    Diff b/w Equals and ==

---
+++ Equals.java
@@ -0,0 +1,11 @@
+package com.hello;
+
+public class Equals {
+	public static void main(String args[]) {
+	String s1="Sachin";
+	String s2=new String("Sachin");
+	System.out.println(s1.equals(s2));
+	System.out.println(s1==s2);
+	}
+
+}
